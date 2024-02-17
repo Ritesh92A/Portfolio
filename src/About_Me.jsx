a1c2b215a32402fa6a573587702bd1459bf8c9a3
Ritesh92A <gaurritesh82@gmail.com>
2024-02-17 20:57:12 +0530

    created navbar

---
+++ src/About_Me.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About_Me() {
+  return (
+    <div>About Me</div>
+  )
+}
+
+export default About_Me
---
+++ src/Nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Nav(){
+    return(
+        <>
+        <nav>
+            <ul>        
+            <img src="image/r1.png" alt="HTML tutorial"></img>
+            <li><a href="http://localhost:5173/">Home</a></li>
+            <li><a href="#Certificates">Certificates</a></li>
+            <li><a href="#Projects">Projects </a></li>
+            <li><a href="#Contact Me">Contact Me</a></li>
+            <li><a href="#Hire Me">Hire Me </a></li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default Nav
